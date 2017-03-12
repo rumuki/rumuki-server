0a5cc89a9add024b2d862ec377eb41295092ab0c
Nathan Kot <me@nathankot.com>
2017-03-12 18:18:43 +0900

    Update last update value in the device update handler

--- src/Handler/DeviceUpdate.hs
+++ src/Handler/DeviceUpdate.hs
@@ -17,6 +17,13 @@ postDeviceUpdateR :: Handler Value
 postDeviceUpdateR = do
   now <- liftIO getCurrentTime
   GETRequest uids keyFingerprint <- requireJsonBody
+
+  -- Update the last updated value on the device:
+  maybeDevice <- runDB $ selectFirst [ DeviceKeyFingerprint ==. keyFingerprint ] []
+  case maybeDevice of
+    Just (Entity did _) -> runDB $ update did [DeviceUpdated =. Just now]
+    _ -> return ()
+
   detections <- runDB $ selectList [ ScreenCaptureDetectionAffectedDeviceKeyFingerprint ==. keyFingerprint ] []
   grants <- runDB $ selectList [ PlaybackGrantRecordingUID <-. uids , PlaybackGrantExpires >. now ] []
   sendResponseStatus status200 $ object [ "playbackGrants" .= grants
