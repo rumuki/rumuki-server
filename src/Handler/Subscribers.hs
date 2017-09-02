ed28e677e32bab1f97d0ebe33de2161d9d5b04a3
Nathan Kot <me@nathankot.com>
2017-09-02 12:52:53 +1200

    Round subscriber creation date to nearest hour
    This makes a lot harder to match access log timestamps, IP addresses to email
    addresses.

--- src/Handler/Subscribers.hs
+++ src/Handler/Subscribers.hs
@@ -1,5 +1,6 @@
 module Handler.Subscribers where
 
+import           Data.Time.Clock
 import           Import
 import           Text.Email.Validate (canonicalizeEmail)
 
@@ -9,7 +10,12 @@ postSubscribersR = do
   subscriber' <- requireJsonEntity [] Nothing :: Handler Subscriber
   email' <- fromMaybeM (invalidArgsI [MsgInvalidEmailAddress]) $
     return . canonicalizeEmail . encodeUtf8 $ subscriberEmail subscriber'
+
+  let roundedNow = UTCTime { utctDay = utctDay now
+                           , utctDayTime = secondsToDiffTime . (*) (60 * 60) . round $
+                                           (utctDayTime now) / (60 * 60) }
+
   let subscriber = subscriber' { subscriberEmail = decodeUtf8 email'
-                               , subscriberCreated = Just now }
+                               , subscriberCreated = Just roundedNow }
   _ <- runDB $ insertUnique subscriber
   sendResponseStatus status201 $ object [ "subscriber" .= subscriber ]
